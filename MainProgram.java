2963c143067ca28f8052a300939f5ac0ee55eeee
unknown <su1715@sookmyung.ac.kr>
2020-12-02 11:28:39 +0900

    commit test

--- MainProgram.java
+++ MainProgram.java
@@ -25,7 +25,50 @@ public class MainProgram {
     public void runModule(int studentID) {
         if (studentID == 11530)
             System.out.println("No. It is the professor ID.\n");
+        else if (studentID == 1715813)
+        	hw_1715813();
         else
             System.out.println("To be developed...\n");
+    }
+    
+    public void hw_1715813() {
+    	int result;
+    	int num1;
+    	int num2;
+    	Scanner scan = new Scanner(System.in);
+    	int menuNumber = printMenu();
+    	
+    	System.out.print("input two numbers: ");
+    	num1 = scan.nextInt();
+    	num2 = scan.nextInt();
+        
+    	if (menuNumber == 1)
+    		result = max_1715813(num1, num2);
+    	else
+    		result = min_1715813(num1, num2);
+    	System.out.println("Result: " + result);
+    	System.out.println();
+    }
+    
+    public int printMenu() {
+    	Scanner scan = new Scanner(System.in);
+    	System.out.println("1. Calculate max");
+    	System.out.println("2. Calculate min");
+    	System.out.print("Enter menu number: ");
+    	return scan.nextInt();
+    }
+    
+    private int max_1715813(int num1, int num2) {
+    	if(num1 > num2)
+    		return num1;
+    	else 
+    		return num2;
+    }
+    
+    private int min_1715813(int num1, int num2) {
+    	if(num1 < num2)
+    		return num1;
+    	else 
+    		return num2;
     }
 }
